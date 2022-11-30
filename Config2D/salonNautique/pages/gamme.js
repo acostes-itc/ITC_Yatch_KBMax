fae06358b5299ba6de9cb58b9f052aef3240264a
rtheveu <117286309+rtheveu@users.noreply.github.com>
2022-11-30 16:56:44 +0100

    ajout js page modele

---
+++ Config2D/salonNautique/pages/gamme.js
@@ -0,0 +1,25 @@
+const images = new Array(
+    "../images/evasion.png",
+    "../images/elegance.png",
+    "../images/prestige.png"
+);
+
+function setElement(param){
+
+  
+
+    document.getElementById('src').src = images[param];
+}
+
+$(document).ready(function(){
+
+    $("#elegance").click(function(){ setElement($(this).val()); });
+    
+    $("#evasion").click(function(){ setElement($(this).val()); });
+
+    $("#prestige").click(function(){ setElement($(this).val()); });
+
+
+});
+
+//setElement(0);
---
+++ Config2D/salonNautique/pages/modele.js
@@ -0,0 +1,25 @@
+const images = new Array(
+    "../images/e1250.jpg",
+    "../images/e1500.jpg",
+    "../images/e1750.png"
+);
+
+function setElement(param){
+
+  
+
+    document.getElementById('src').src = images[param];
+}
+
+$(document).ready(function(){
+
+    $("#e1250").click(function(){ setElement($(this).val()); });
+    
+    $("#e1500").click(function(){ setElement($(this).val()); });
+
+    $("#e1750").click(function(){ setElement($(this).val()); });
+
+
+});
+
+//setElement(0);
